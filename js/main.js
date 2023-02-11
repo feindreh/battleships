39e4164707f122844fd58c854b2f0fe10985aa8a
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-11 18:32:00 +0100

    set up simple board with colors and fixed ships

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import Player from './factory/player.js';
+import shipfactory from './factory/ships.js';
 
 const Player1 = Player('Player1');
 
@@ -8,7 +9,16 @@ const makeSquare = (square) => {
   const div = document.createElement('div');
   div.className = 'square';
   div.addEventListener('click', () => {
-    console.log(square);
+    if (square.hit) {
+      console.log('square already hit');
+    } else {
+      square.getHit();
+      if (square.ship === null) {
+        div.style.backgroundColor = 'grey';
+      } else {
+        div.style.backgroundColor = 'red';
+      }
+    }
   });
   return div;
 };
@@ -19,10 +29,18 @@ const makeRow = () => {
   return row;
 };
 
-for (let y = 7; y >= 0; y--) {
+for (let y = 9; y >= 0; y--) {
   const row = makeRow();
-  for (let x = 0; x <= 7; x++) {
+  for (let x = 0; x <= 9; x++) {
     row.append(makeSquare(Player1.playerBoard.board[x][y]));
   }
   container.append(row);
 }
+
+const ships = shipfactory();
+
+Player1.playerBoard.placeShip(ships.battleship(), [0, 0], 'right');
+Player1.playerBoard.placeShip(ships.carrier(), [0, 2], 'right');
+Player1.playerBoard.placeShip(ships.destroyer(), [0, 4], 'right');
+Player1.playerBoard.placeShip(ships.patrolBoat(), [0, 6], 'right');
+Player1.playerBoard.placeShip(ships.submarine(), [0, 8], 'right');
