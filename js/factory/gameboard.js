04fd99f1c7457bc3b3b4ef1beecd43351789e036
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-11 12:52:19 +0100

    add placeship

--- js/factory/gameboard.js
+++ js/factory/gameboard.js
@@ -1,7 +1,9 @@
-const square = () => ({
+const square = (x, y) => ({
   // has information
   // if it was hit or not
   // if it has a ship on it and what ship it has on it
+  x,
+  y,
   hit: false,
   ship: null,
 });
@@ -11,7 +13,7 @@ const makeBoard = () => {
   for (let x = 0; x <= 7; x++) {
     const row = [];
     for (let y = 0; y <= 7; y++) {
-      row.push(square());
+      row.push(square(x, y));
     }
     newBoard.push(row);
   }
@@ -20,6 +22,17 @@ const makeBoard = () => {
 
 const gameBoard = () => ({
   board: makeBoard(),
+  placeShip(ship, start, direction) {
+    // direction => up or right
+    let x = start[0];
+    let y = start[1];
+
+    for (let i = 0; i < ship.length; i++) {
+      this.board[x][y].ship = ship;
+      if (direction === 'up') { y++; }
+      if (direction === 'right') { x++; }
+    }
+  },
 });
 
 export default gameBoard;
