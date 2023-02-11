32be809b308b4442f7d5af0fd296b31050e64d5a
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-11 12:52:27 +0100

    add placeship test

--- tests/board.test.js
+++ tests/board.test.js
@@ -9,3 +9,39 @@ test('is a 8x8 grid', () => {
     expect(row.length).toBe(8)
   ));
 });
+
+test('place Ship length 3 right', () => {
+  const Board = gameBoard();
+  const mock = {
+    length: 3,
+  };
+  Board.placeShip(mock, [0, 0], 'right');
+
+  Board.board.forEach((row) => {
+    row.forEach((square) => {
+      if ((square.x === 0 || square.x === 1 || square.x === 2) && square.y === 0) {
+        expect(square.ship).toBe(mock);
+      } else {
+        expect(square.ship).toBe(null);
+      }
+    });
+  });
+});
+
+test('place Ship length 3 up', () => {
+  const Board = gameBoard();
+  const mock = {
+    length: 3,
+  };
+  Board.placeShip(mock, [3, 3], 'up');
+
+  Board.board.forEach((row) => {
+    row.forEach((square) => {
+      if ((square.y === 3 || square.y === 4 || square.y === 5) && square.x === 3) {
+        expect(square.ship).toBe(mock);
+      } else {
+        expect(square.ship).toBe(null);
+      }
+    });
+  });
+});
