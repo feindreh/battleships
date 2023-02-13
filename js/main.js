c8003d16e8216f187987bec72c71ac16e116fc76
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-13 23:40:07 +0100

    add shipplacement

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import shipfactory from './factory/ships.js';
 import Gamelogic from './Gamelogic.js';
 import Player from './factory/player.js';
 import computerAi from './Ai.js';
+import { makeShowBoard } from './dom.js';
 
 const ships = shipfactory();
 
@@ -15,9 +16,40 @@ Gamelogic.player2 = Player2;
 Gamelogic.AI = AI;
 Gamelogic.ships = ships;
 
-Gamelogic.placeShips(Player1);
-Gamelogic.placeShips(Player2);
+function placeRandomShips(player) {
+  player.playerBoard.placeShip(ships.battleship(), [2, 0], 'right');
+  player.playerBoard.placeShip(ships.carrier(), [2, 2], 'right');
+  player.playerBoard.placeShip(ships.destroyer(), [2, 4], 'right');
+  player.playerBoard.placeShip(ships.patrolBoat(), [0, 6], 'right');
+  player.playerBoard.placeShip(ships.submarine(), [0, 8], 'right');
+}
 
-// After Ship placement
+function placeShips(player) {
+  if (player.human === false) { placeRandomShips(player); } else {
+    place(player);
+  }
+  // click => show new ship placed
 
-// Gamelogic.updateGameboard(Player1, Player2, Gamelogic);
+  // confirm => return value (Promise ??)
+}
+
+function refresh(player) {
+  const container = document.querySelector('#container');
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+  container.append(makeShowBoard(player, cb));
+}
+
+function cb(player, x, y) {
+  player.playerBoard.placeShip(ships.battleship(), [x, y], 'right');
+  refresh(player);
+}
+
+function place(player) {
+  const container = document.querySelector('#container');
+  container.append(makeShowBoard(player, cb));
+}
+
+placeShips(Player1);
+placeShips(Player2);
