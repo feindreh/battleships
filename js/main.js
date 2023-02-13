0785a96537d32d59be3d869fc41ce1a4d34597d2
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-13 20:42:30 +0100

    typo

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const ships = shipfactory();
 
 const Player1 = Player('Player');
 const Player2 = Player('Computer');
+const AI = computerAi(Gamelogic, Player1);
 
 Player1.playerBoard.placeShip(ships.battleship(), [0, 0], 'right');
 Player1.playerBoard.placeShip(ships.carrier(), [0, 2], 'right');
@@ -19,8 +20,6 @@ Player2.playerBoard.placeShip(ships.carrier(), [0, 2], 'right');
 Player2.playerBoard.placeShip(ships.destroyer(), [0, 4], 'right');
 Player2.playerBoard.placeShip(ships.patrolBoat(), [0, 6], 'right');
 Player2.playerBoard.placeShip(ships.submarine(), [0, 8], 'right');
-
-const AI = computerAi(Gamelogic, Player1);
 
 Gamelogic.player1 = Player1;
 Gamelogic.player2 = Player2;
