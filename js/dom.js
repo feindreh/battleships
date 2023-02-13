5624e15a45aa6c23574bc167a256b2924838e8b6
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-13 23:39:40 +0100

    add makePlace function

--- js/dom.js
+++ js/dom.js
@@ -19,6 +19,21 @@ const makeSquare = (square, logik) => {
   return div;
 };
 
+const makePlace = (square, player, cb) => {
+  const div = document.createElement('div');
+
+  div.className = 'square';
+  if (square.ship !== null) {
+    div.className = 'shipplaced';
+  }
+
+  div.addEventListener('click', () => {
+    console.log(square);
+    cb(player, square.x, square.y);
+  });
+  return div;
+};
+
 const makeRow = () => {
   const row = document.createElement('div');
   row.className = 'row';
@@ -40,6 +55,22 @@ const makePlayerBoard = (player, logik) => {
 
   return container;
 };
+
+export function makeShowBoard(player, cb) {
+  const container = document.createElement('div');
+  container.id = 'showboard';
+  container.className = 'gameBoard';
+
+  for (let y = 9; y >= 0; y--) {
+    const row = makeRow();
+    for (let x = 0; x <= 9; x++) {
+      row.append(makePlace(player.playerBoard.board[x][y], player, cb));
+    }
+    container.append(row);
+  }
+
+  return container;
+}
 
 export function updateGameboard(player1, player2, logik) {
   const container = document.querySelector('#container');
