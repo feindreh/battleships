0761f539e01fdb1ab2daa8e22b6c8a087bb820d9
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-13 14:38:41 +0100

    fix getRamdomFreeField grid from 8 to 10

--- js/main.js
+++ js/main.js
@@ -70,13 +70,13 @@ Player2.playerBoard.placeShip(ships.patrolBoat(), [0, 6], 'right');
 Player2.playerBoard.placeShip(ships.submarine(), [0, 8], 'right');
 
 const getRandomFreeField = (enemy) => {
+  let count = 0;
   let x = Math.floor(Math.random() * 8);
   let y = Math.floor(Math.random() * 8);
-
   while (enemy.playerBoard.board[x][y].hit === true) {
-    if (x === 7) { x = 0; y += 1; } else {
-      x += 1;
-    }
+    if (x === 9 && y === 9) { x = 0; y = 0; } else if (x === 9) { x = 0; y += 1; } else { x += 1; }
+    count++;
+    if (count >= 100) { throw 'no field left to hit'; }
   }
   return [x, y];
 };
