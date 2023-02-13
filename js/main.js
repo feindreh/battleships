2f76d20e20d074e15cacbdd00cb4def221df5042
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-13 20:32:06 +0100

    ai target finding more random

--- js/main.js
+++ js/main.js
@@ -21,12 +21,18 @@ Player2.playerBoard.placeShip(ships.submarine(), [0, 8], 'right');
 
 const getRandomFreeField = (enemy) => {
   let count = 0;
-  let x = Math.floor(Math.random() * 8);
-  let y = Math.floor(Math.random() * 8);
+  let x = Math.floor(Math.random() * 10);
+  let y = Math.floor(Math.random() * 10);
+  while (enemy.playerBoard.board[x][y].hit === true) {
+    x = Math.floor(Math.random() * 10);
+    y = Math.floor(Math.random() * 10);
+    count++;
+    if (count >= 20) { break; }
+  }
   while (enemy.playerBoard.board[x][y].hit === true) {
     if (x === 9 && y === 9) { x = 0; y = 0; } else if (x === 9) { x = 0; y += 1; } else { x += 1; }
     count++;
-    if (count >= 100) { throw 'no field left to hit'; }
+    if (count >= 130) { throw 'no field left to hit'; }
   }
   return [x, y];
 };
