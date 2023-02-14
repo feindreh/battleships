388dae473b42e970b551533b4a988b8d0761fe1d
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-14 10:20:26 +0100

    add placeship with click functions

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import shipfactory from './factory/ships.js';
 import Gamelogic from './Gamelogic.js';
 import Player from './factory/player.js';
 import computerAi from './Ai.js';
-import { makeShowBoard } from './dom.js';
+import { showBoard } from './dom.js';
 
 const ships = shipfactory();
 
@@ -16,40 +16,35 @@ Gamelogic.player2 = Player2;
 Gamelogic.AI = AI;
 Gamelogic.ships = ships;
 
-function placeRandomShips(player) {
-  player.playerBoard.placeShip(ships.battleship(), [2, 0], 'right');
-  player.playerBoard.placeShip(ships.carrier(), [2, 2], 'right');
-  player.playerBoard.placeShip(ships.destroyer(), [2, 4], 'right');
-  player.playerBoard.placeShip(ships.patrolBoat(), [0, 6], 'right');
-  player.playerBoard.placeShip(ships.submarine(), [0, 8], 'right');
+// Phase 1 Place
+
+function placeBattleship(player) {
+  showBoard(player, plCarrier);
 }
 
-function placeShips(player) {
-  if (player.human === false) { placeRandomShips(player); } else {
-    place(player);
-  }
-  // click => show new ship placed
+placeBattleship(Player1);
 
-  // confirm => return value (Promise ??)
+function plCarrier(square, player) {
+  player.playerBoard.placeShip(ships.carrier(), [square.x, square.y], 'right');
+  showBoard(player, (square, player) => { plBattle(square, player); });
 }
 
-function refresh(player) {
-  const container = document.querySelector('#container');
-  while (container.firstChild) {
-    container.firstChild.remove();
-  }
-  container.append(makeShowBoard(player, cb));
+function plBattle(square, player) {
+  player.playerBoard.placeShip(ships.battleship(), [square.x, square.y], 'right');
+  showBoard(player, (square, player) => { plSub(square, player); });
 }
 
-function cb(player, x, y) {
-  player.playerBoard.placeShip(ships.battleship(), [x, y], 'right');
-  refresh(player);
+function plSub(square, player) {
+  player.playerBoard.placeShip(ships.submarine(), [square.x, square.y], 'right');
+  showBoard(player, (square, player) => { pldestroyer(square, player); });
 }
 
-function place(player) {
-  const container = document.querySelector('#container');
-  container.append(makeShowBoard(player, cb));
+function pldestroyer(square, player) {
+  player.playerBoard.placeShip(ships.destroyer(), [square.x, square.y], 'right');
+  showBoard(player, (square, player) => { plpatrol(square, player); });
 }
 
-placeShips(Player1);
-placeShips(Player2);
+function plpatrol(square, player) {
+  player.playerBoard.placeShip(ships.patrolBoat(), [square.x, square.y], 'right');
+  showBoard(player, (square) => { console.log(square); });
+}
