5e682f7881ef0b60895af04d7eab682e0600fbc9
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-14 11:20:58 +0100

    add undo function with bloated switch statements

--- js/main.js
+++ js/main.js
@@ -24,27 +24,139 @@ function placeBattleship(player) {
 
 placeBattleship(Player1);
 
+const undo = document.querySelector('#undo');
+const rotate = document.querySelector('#rotate');
+let lastChange = 'nothing';
+const activePlayer = Player1;
+let lastCallback = plCarrier;
+undo.addEventListener('click', () => {
+  activePlayer.playerBoard.removeShip(lastChange);
+  if (lastChange === 'nothing') { console.log('cant undo nothing'); } else {
+    lastChange = getlastChangeminus(lastChange);
+    lastCallback = getlastCallback(lastChange);
+  }
+  showBoard(activePlayer, lastCallback);
+});
+
 function plCarrier(square, player) {
   player.playerBoard.placeShip(ships.carrier(), [square.x, square.y], 'right');
+  lastChange = 'carrier';
+  lastCallback = plCarrier;
   showBoard(player, (square, player) => { plBattle(square, player); });
 }
 
 function plBattle(square, player) {
   player.playerBoard.placeShip(ships.battleship(), [square.x, square.y], 'right');
+  lastChange = 'battleship';
+  lastCallback = plBattle;
   showBoard(player, (square, player) => { plSub(square, player); });
 }
 
 function plSub(square, player) {
   player.playerBoard.placeShip(ships.submarine(), [square.x, square.y], 'right');
+  lastChange = 'submarine';
+  lastCallback = plSub;
   showBoard(player, (square, player) => { pldestroyer(square, player); });
 }
 
 function pldestroyer(square, player) {
   player.playerBoard.placeShip(ships.destroyer(), [square.x, square.y], 'right');
+  lastChange = 'destroyer';
+  lastCallback = pldestroyer;
   showBoard(player, (square, player) => { plpatrol(square, player); });
 }
 
 function plpatrol(square, player) {
   player.playerBoard.placeShip(ships.patrolBoat(), [square.x, square.y], 'right');
+  lastChange = 'patrol boat';
+  lastCallback = plpatrol;
   showBoard(player, (square) => { console.log(square); });
+}
+
+function getlastChangeminus(name) {
+  let current;
+  switch (name) {
+    case 'carrier':
+      current = 1;
+      break;
+    case 'battleship':
+      current = 2;
+      break;
+    case 'submarine':
+      current = 3;
+      break;
+    case 'destroyer':
+      current = 4;
+      break;
+    case 'patrol boat':
+      current = 5;
+      break;
+    default:
+      throw 'switch name broken';
+  }
+  switch (current) {
+    case 1:
+      return 'nothing';
+      break;
+    case 2:
+      return 'carrier';
+      break;
+    case 3:
+      return 'battleship';
+      break;
+    case 4:
+      return 'submarine';
+      break;
+    case 5:
+      return 'destroyer';
+      break;
+    default:
+      throw 'switch current broken';
+  }
+}
+
+function getlastCallback(name) {
+  console.log(name);
+  let current;
+  switch (name) {
+    case 'nothing':
+      current = 0;
+      break;
+    case 'carrier':
+      current = 1;
+      break;
+    case 'battleship':
+      current = 2;
+      break;
+    case 'submarine':
+      current = 3;
+      break;
+    case 'destroyer':
+      current = 4;
+      break;
+    case 'patrol boat':
+      current = 5;
+      break;
+    default:
+      throw 'switch callback name broken';
+  }
+  switch (current) {
+    case 0:
+      return plCarrier;
+      break;
+    case 1:
+      return plBattle;
+      break;
+    case 2:
+      return plSub;
+      break;
+    case 3:
+      return pldestroyer;
+      break;
+    case 4:
+      return plpatrol;
+      break;
+    default:
+      throw 'switch callback current broken';
+  }
 }
